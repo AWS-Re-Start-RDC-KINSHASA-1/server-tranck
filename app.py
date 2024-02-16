60a96f05a6cc438ff35906fc5ca8291f2a2d57b2
Divin Omas <divinomas19@gmail.com>
2024-02-16 16:00:36 +0100

    5010

--- app.py
+++ app.py
@@ -28,4 +28,4 @@ def logout():
     return redirect(url_for("login"))
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5001, debug=True)
+    app.run(host='0.0.0.0', port=5010, debug=True)
